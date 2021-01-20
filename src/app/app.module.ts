1eb10af467fbdc715519313066dfcfc8ea1e14da
blakes-mike <blakesmike12@gmail.com>
2021-01-20 11:41:42 +0100

    do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponet } from './course.component';
+import { CourseComponent } from './course/course.component';
+import { CourseService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponet,
+    CourseComponent,
+    AuthorsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CourseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
